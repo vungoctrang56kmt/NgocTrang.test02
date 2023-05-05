b488acdd0ef0dcd95abfa96252e66635ae7a6dcd
nusyu <nusyu@TRANG>
2023-05-05 15:09:56 +0700

    Add project files.

---
+++ NgocTrang.test02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NgocTrang.test02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Xin Chao");
+
+        }
+    }
+}
